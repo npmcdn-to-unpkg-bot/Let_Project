195d87e6987632469f36581d8318560e9efe7456
Sonya Panich <sonyapanich@gmail.com>
2016-02-24 14:12:33 -0800

    something

--- CI-master-2.2.3/application/config/database.php
+++ CI-master-2.2.3/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'root';
+$db['default']['database'] = 'letgo';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
--- CI-master-2.2.3/application/models/let.php
+++ CI-master-2.2.3/application/models/let.php
@@ -6,8 +6,19 @@ class Let extends CI_Model
 		parent::__construct();
 	}
 
-	public function get_user(){
+	public function get_user($user_info){
+		return $this->db->query("SELECT * FROM users WHERE username='{$user_info['username']}'
+			                               AND password = '{$user_info['password']}'")->row_array();
+
 		
+	}
+	public function insert_user($user_info){
+		$insert_user= "INSERT INTO users (first_name, last_name, username, email, password, created_at) 
+		               VALUES (?, ?, ?, ?, ?, NOW())";
+		$values- (array($user_info['first_name'], $user_info['last_name'], $user_info['username'], $user_info['email'], $user_info['password']));
+		$this->db->query($insert_query, $values);
+		return $this->db->insert_id();
+
 	}
 
 }
