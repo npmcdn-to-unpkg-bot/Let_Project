6dc6dd244713ab686633cd85147db08ddfe07c81
Alexandra Anderson <aliganderson@gmail.com>
2016-02-24 13:59:35 -0800

    added styles sheet and other css to welcome.php

---
+++ CI-master-2.2.3/application/views/welcome.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+  <title>Let it Go</title>  
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+  integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+  integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">
+  </script>
+  <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/assets/css/style.css">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js'></script>
+  <script src="js/jquery.responsiveiframe.js"></script>
+  <script src="js/ios-orientationchange-fix.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $(".dropdown-toggle").dropdown();
+    });
+
+  </script>
+  <link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
+</head>
+<body>
+  <div class="background-wrap">
+    <video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted">
+        <source src="/assets/pix/CliffAngry.mp4" type="video/mp4">
+          Video not supported
+    </video>
+  </div>
+<div class="content">
+
+  <h1>Welcome</h1>
+  <p>Welcome to let it go, the place to express yourself.</p>
+
+
+</div>
+
+</body>
+</html>
